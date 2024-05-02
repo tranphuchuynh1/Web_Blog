ded784da01d220fe146a2dcd2f377d449575ebb6
trongchoinak <vibana.org@gmail.com>
2024-05-02 10:51:20 +0700

    trongupdate1

---
+++ Web_Blog/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web_Blog.Models;
+namespace Web_Blog.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+       
+    }
+
+}
---
+++ Web_Blog/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Blog.Models
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; } 
+        public string? username { get; set; }
+        
+        public string? password { get; set; }
+
+        public string? Email { get; set; }
+        public byte[]  Avartar { get; set; }
+
+    }
+}
