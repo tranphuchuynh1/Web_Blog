55fa9d179faa925c2f64c44255f14afc34d3f40b
trongchoinak <vibana.org@gmail.com>
2024-05-02 10:51:20 +0700

    create and update databasetrongupdate1

--- Web_Blog/Program.cs
+++ Web_Blog/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using Web_Blog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// Register database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
