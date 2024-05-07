7c0e97428f88158e4ffaa19c06f0dcb6760a14fc
tranphuchuynh1 <tranphuchuynh1@gmail.com>
2024-05-07 20:21:13 +0700

    huynhupdate(07/05)

---
+++ Web_Blog/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Blog.Data;
+using Web_Blog.Models;          
+
+namespace Web_Blog.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult PageProfile()
+        {
+            return View();
+        }
+
+
+
+    }
+}
