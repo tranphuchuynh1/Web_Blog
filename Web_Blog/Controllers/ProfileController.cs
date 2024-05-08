24439354f3916aa1cb9f60941577c7229ae515aa
tranphuchuynh1 <tranphuchuynh1@gmail.com>
2024-05-09 01:03:33 +0700

    huynhupdate(1:04 AM 09/05)

--- Web_Blog/Controllers/ProfileController.cs
+++ Web_Blog/Controllers/ProfileController.cs
@@ -11,10 +11,12 @@ namespace Web_Blog.Controllers
         {
             string userName = HttpContext.Session.GetString("username");
             string email = HttpContext.Session.GetString("Email");
+            string iduser = HttpContext.Session.GetString("idUser");
 
             // Gán tên người dùng vào ViewBag để truyền sang view
             ViewBag.UserName = userName;
             ViewBag.EMAIL = email;
+            ViewBag.Iduser = iduser;
             return View();
         }
 
