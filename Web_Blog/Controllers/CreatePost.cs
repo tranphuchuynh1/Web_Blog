c6060941c8256420a6adb80f609382fc307fc746
trongchoinak <vibana.org@gmail.com>
2024-05-17 22:28:58 +0700

    updateductrong17/5

--- Web_Blog/Controllers/CreatePost.cs
+++ Web_Blog/Controllers/CreatePost.cs
@@ -3,11 +3,9 @@ using Web_Blog.Data;
 using Web_Blog.Models;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using System.Diagnostics;
-using Web_Blog.Models.Interfaces;
-using Web_Blog.Models.Services;
+using Microsoft.AspNetCore.Http;
 using System;
-
+using System.Linq;
 
 namespace Web_Blog.Controllers
 {
@@ -21,15 +19,21 @@ namespace Web_Blog.Controllers
             _db = db;
             _environment = environment;
         }
+
         [HttpPost]
         public ActionResult Create(string Category, string Title, string Content, IFormFile ImageURL)
         {
-
             if (ModelState.IsValid)
             {
                 DateTime CreatedAt = DateTime.Now;
                 string userName = HttpContext.Session.GetString("username");
                 int? userId = HttpContext.Session.GetInt32("idUser");
+
+                if (userId == null)
+                {
+                    return Json(new { success = false, message = "User not logged in." });
+                }
+
                 Post post = new Post
                 {
                     Category = Category,
@@ -38,31 +42,30 @@ namespace Web_Blog.Controllers
                     CreatedAt = CreatedAt,
                     idUser = userId.Value
                 };
+
                 if (ImageURL != null && ImageURL.Length > 0)
                 {
                     string fileName = Title + Path.GetExtension(ImageURL.FileName);
-                    string filePath = @"wwwroot\ProductImages\" + fileName;
+                    string uploadsFolder = Path.Combine(_environment.WebRootPath, "ProductImages");
+                    string filePath = Path.Combine(uploadsFolder, fileName);
 
-
-                    var directoryLocation = Path.Combine(Directory.GetCurrentDirectory(), filePath);
-                    FileInfo file = new FileInfo(directoryLocation);
-
-                    var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
-                    using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
                     {
                         ImageURL.CopyTo(fileStream);
                     }
+
                     var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
-                    post.ImageURL = baseUrl + "/ProductImages/" + fileName;
+                    post.ImageURL = $"{baseUrl}/ProductImages/{fileName}";
                 }
+
                 _db.Posts.Add(post);
                 _db.SaveChanges();
+
                 // Điều hướng người dùng đến trang hiển thị bài viết đã đăng
                 return Json(new { success = true, imageUrl = post.ImageURL, category = post.Category, title = post.Title, content = post.Content, createdAt = post.CreatedAt.ToString("yyyy-MM-dd HH:mm:ss"), username = userName });
             }
             else
             {
-
                 return Json(new { success = false, message = "Model state is invalid." });
             }
         }
